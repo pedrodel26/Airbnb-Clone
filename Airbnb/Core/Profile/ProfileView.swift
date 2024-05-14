99f653ef4d4ebc22bff4d5bb2d64cffe3f6ad0b8
pedrodel26 <pedromodesto26@gmail.com>
2024-05-14 16:52:26 -0300

    Finished ProfileView Part

--- Airbnb/Core/Profile/ProfileView.swift
+++ Airbnb/Core/Profile/ProfileView.swift
@@ -9,25 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Profile")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
+        VStack(alignment: .leading, spacing: 32) {
             
-            Text("Login to start planning your next trip")
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Profile")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Login to start planning your next trip")
+            }
+            
+            Button(action: {
+                print("Login")
+            }, label: {
+                Text("Log in")
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .frame(width: 360, height: 48)
+                    .background(.pink)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            })
+            
+            HStack {
+                Text("Don't have an account?")
+                
+                Text("Sign Up")
+                    .fontWeight(.semibold)
+                    .underline()
+            }
         }
-        
-        Button(action: {
-            print("Login")
-        }, label: {
-            Text("Log in")
-                .foregroundStyle(.white)
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .frame(width: 360, height: 48)
-                .background(.pink)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-        })
     }
 }
 

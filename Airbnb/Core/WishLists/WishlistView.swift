1972b12cc1b8e0e6b2cd2377e50cb7a11545bb6d
pedrodel26 <pedromodesto26@gmail.com>
2024-05-16 11:03:53 -0300

    Created a Wishlist View - very simple!

--- Airbnb/Core/WishLists/WishlistView.swift
+++ Airbnb/Core/WishLists/WishlistView.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 struct WishlistView: View {
     var body: some View {
-        VStack {
-            VStack {
-                Text("Log in to view your wishlists")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 32) {
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Log in to view your wishlists")
+                            .font(.headline)
+                        
+                        Text("You can create, view or edit wishlists once you've logged in")
+                    }
+                    
+                    Button(action: {
+                        print("Login")
+                    }, label: {
+                        Text("Log in")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 48)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                    
+                    Spacer()
+                }
+                .padding()
+                .navigationTitle("Wishlist")
             }
         }
     }
